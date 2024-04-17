fa88dcac93582ec9b7ecfa046a5c1abdb2f61e12
m3ruem <109645090+m3ruem@users.noreply.github.com>
2024-04-17 12:14:14 +0800

    PushedbyCJ

---
+++ lib/network/recipe_service.dart
@@ -0,0 +1,32 @@
+import 'dart:developer';
+import 'package:http/http.dart';
+
+const String apiKey = '328687a01cc46d63f0d2d64c1d5e4a5b';
+const String apiId = '101c515f';
+const String apiUrl = 'https://api.edamam.com/search';
+
+class RecipeService {
+  // 1
+  Future getData(String url) async {
+    // 2
+    final response = await get(Uri.parse(url));
+    // 3
+    if (response.statusCode == 200) {
+      // 4
+      return response.body;
+    } else {
+      // 5
+      log(response.body);
+    }
+  }
+
+  // TODO: Add getRecipes
+  // 1
+  Future<dynamic> getRecipes(String query, int from, int to) async {
+    // 2
+    final recipeData = await getData(
+        '$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to');
+    // 3
+    return recipeData;
+  }
+}
